47a368941fce50390085f5bbb59bf40c24be81e3
Imran-D-Mahmood <48464030+Imran-D-Mahmood@users.noreply.github.com>
2019-12-14 05:45:56 -0800

    Add files via upload

--- CCFSWeb/index.php
+++ CCFSWeb/index.php
@@ -4,27 +4,27 @@
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <title>CCFS Student Information System</title>
-  <link rel="icon" href="../Resources/dist/img/CCFS_logo.png" type="image/icon type">
+  <link rel="icon" href="Resources/dist/img/CCFS_logo.png" type="image/icon type">
   <!-- Tell the browser to be responsive to screen width -->
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <!-- Font Awesome -->
-  <link rel="stylesheet" href="../Resources/plugins/fontawesome-free/css/all.min.css">
+  <link rel="stylesheet" href="Resources/plugins/fontawesome-free/css/all.min.css">
   <!-- Ionicons -->
   <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
   <!-- Tempusdominus Bbootstrap 4 -->
-  <link rel="stylesheet" href="../Resources/plugins/tempusdominus-bootstrap-4/css/tempusdominus-bootstrap-4.min.css">
+  <link rel="stylesheet" href="Resources/plugins/tempusdominus-bootstrap-4/css/tempusdominus-bootstrap-4.min.css">
   <!-- iCheck -->
-  <link rel="stylesheet" href="../Resources/plugins/icheck-bootstrap/icheck-bootstrap.min.css">
+  <link rel="stylesheet" href="Resources/plugins/icheck-bootstrap/icheck-bootstrap.min.css">
   <!-- JQVMap -->
-  <link rel="stylesheet" href="../Resources/plugins/jqvmap/jqvmap.min.css">
+  <link rel="stylesheet" href="Resources/plugins/jqvmap/jqvmap.min.css">
   <!-- Theme style -->
-  <link rel="stylesheet" href="../Resources/dist/css/adminlte.min.css">
+  <link rel="stylesheet" href="Resources/dist/css/adminlte.min.css">
   <!-- overlayScrollbars -->
-  <link rel="stylesheet" href="../Resources/plugins/overlayScrollbars/css/OverlayScrollbars.min.css">
+  <link rel="stylesheet" href="Resources/plugins/overlayScrollbars/css/OverlayScrollbars.min.css">
   <!-- Daterange picker -->
-  <link rel="stylesheet" href="../Resources/plugins/daterangepicker/daterangepicker.css">
+  <link rel="stylesheet" href="Resources/plugins/daterangepicker/daterangepicker.css">
   <!-- summernote -->
-  <link rel="stylesheet" href="../Resources/plugins/summernote/summernote-bs4.css">
+  <link rel="stylesheet" href="Resources/plugins/summernote/summernote-bs4.css">
   <!-- Google Font: Source Sans Pro -->
   <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700" rel="stylesheet">
   <link rel="stylesheet" type="text/css" href="dist/css/main.css">
@@ -44,38 +44,38 @@
   </a>
 
 <!-- jQuery -->
-<script src="../Resources/plugins/jquery/jquery.min.js"></script>
+<script src="Resources/plugins/jquery/jquery.min.js"></script>
 <!-- jQuery UI 1.11.4 -->
-<script src="../Resources/plugins/jquery-ui/jquery-ui.min.js"></script>
+<script src="Resources/plugins/jquery-ui/jquery-ui.min.js"></script>
 <!-- Resolve conflict in jQuery UI tooltip with Bootstrap tooltip -->
 <script>
   $.widget.bridge('uibutton', $.ui.button)
 </script>
 <!-- Bootstrap 4 -->
-<script src="../Resources/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+<script src="Resources/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
 <!-- ChartJS -->
-<script src="../Resources/plugins/chart.js/Chart.min.js"></script>
+<script src="Resources/plugins/chart.js/Chart.min.js"></script>
 <!-- Sparkline -->
-<script src="../Resources/plugins/sparklines/sparkline.js"></script>
+<script src="Resources/plugins/sparklines/sparkline.js"></script>
 <!-- JQVMap -->
-<script src="../Resources/plugins/jqvmap/jquery.vmap.min.js"></script>
-<script src="../Resources/plugins/jqvmap/maps/jquery.vmap.usa.js"></script>
+<script src="Resources/plugins/jqvmap/jquery.vmap.min.js"></script>
+<script src="Resources/plugins/jqvmap/maps/jquery.vmap.usa.js"></script>
 <!-- jQuery Knob Chart -->
-<script src="../Resources/plugins/jquery-knob/jquery.knob.min.js"></script>
+<script src="Resources/plugins/jquery-knob/jquery.knob.min.js"></script>
 <!-- daterangepicker -->
-<script src="../Resources/plugins/moment/moment.min.js"></script>
-<script src="../Resources/plugins/daterangepicker/daterangepicker.js"></script>
+<script src="Resources/plugins/moment/moment.min.js"></script>
+<script src="Resources/plugins/daterangepicker/daterangepicker.js"></script>
 <!-- Tempusdominus Bootstrap 4 -->
-<script src="../Resources/plugins/tempusdominus-bootstrap-4/js/tempusdominus-bootstrap-4.min.js"></script>
+<script src="Resources/plugins/tempusdominus-bootstrap-4/js/tempusdominus-bootstrap-4.min.js"></script>
 <!-- Summernote -->
-<script src="../Resources/plugins/summernote/summernote-bs4.min.js"></script>
+<script src="Resources/plugins/summernote/summernote-bs4.min.js"></script>
 <!-- overlayScrollbars -->
-<script src="../Resources/plugins/overlayScrollbars/js/jquery.overlayScrollbars.min.js"></script>
+<script src="Resources/plugins/overlayScrollbars/js/jquery.overlayScrollbars.min.js"></script>
 <!-- AdminLTE App -->
-<script src="../Resources/dist/js/adminlte.js"></script>
+<script src="Resources/dist/js/adminlte.js"></script>
 <!-- AdminLTE dashboard demo (This is only for demo purposes) -->
-<script src="../Resources/dist/js/pages/dashboard.js"></script>
+<script src="Resources/dist/js/pages/dashboard.js"></script>
 <!-- AdminLTE for demo purposes -->
-<script src="../Resources/dist/js/demo.js"></script>
+<script src="Resources/dist/js/demo.js"></script>
 </body>
 </html>
