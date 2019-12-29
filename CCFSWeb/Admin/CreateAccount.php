54ad2a7beb40a16235fbb15030259dfa0ea1d209
Paul Perez <45648270+Archaician@users.noreply.github.com>
2019-12-29 18:07:57 +0800

    working

--- CCFSWeb/Admin/CreateAccount.php
+++ CCFSWeb/Admin/CreateAccount.php
@@ -1,3 +1,23 @@
+<?php include 'database.php'; ?>
+<?php 
+	if(isset($_POST['submit'])){
+		
+		
+
+		
+		$query = "INSERT INTO `accounts`(empid, username, password, fname, lname, type) values ('$_POST[empid]', '$_POST[username]', '$_POST[password]', '$_POST[fname]', '$_POST[lname]', '$_POST[type]')";
+		
+		$insert_row = $mysqli->query($query) or die($mysqli->error.__LINE__);
+		
+
+
+	$message = 'Account have been added';
+	
+	}
+	
+	
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -28,48 +48,82 @@
 </head>
 <body>
 
-	<div>
-		<!--<div class="container-login100"> -->
-		<div>
-			<div>
-				<form class="login100-form validate-form" action="CreateAccount.php">
-					<span class="login100-form-title p-b-33">
-						Create Account
-					</span>
-					<div class="wrap-input100 validate-input">
-						Account Type:
-						<select>
-							<option value="ACCOUNTING">ACCOUNTING</option>
-							<option value="REGISTRAR">REGISTRAR</option>
-						</select><br>
-					</div>
-					<div class="wrap-input100 validate-input" data-validate="REQUIRED!">
-					Employee ID:
-					<input class="input100" type="text" name="empid"><br>
-				</div>
-				<div class="wrap-input100 validate-input">
-					First Name:
-					<input class="input100" type="text" name="firstname"><br>
-				</div>
-				<div class="wrap-input100 validate-input">
-					Last Name:
-					<input class="input100" type="text" name="lastname"><br>
-				</div>
-				<div class="wrap-input100 validate-input">
-					Username:
-					<input class="input100" type="text" name="username"><br>
-				</div>
-				<div class="wrap-input100 validate-input">
-					Password:
-					<input class="input100" type="password" name="psw"><br>
-				</div>
-					<div class="wrap-input100 validate-input">
-					Confirm Password:
-					<input class="input100" type="password" name="confpsw"><br>
-					</div>
-					<div class="container-login100-form-btn m-t-20">
-					<input type="submit" value="Create">
-				</div>
+<body class="hold-transition register-page">
+<div class="register-box">
+
+  <div class="card">
+  	    <img src="../Resources/dist/img/CCFS_Logo.png" alt="CCFS Logo" id="ccfs_logoAcc" class="animated animatedFadeInUp fadeInUp" >
+    <div class="card-body register-card-body">
+      <p class="login-box-msg">Register a new account</p>
+
+      <form method="post" action="CreateAccount.php">
+        <div class="input-group mb-3">
+          <input type= "text" name ="empid" class="form-control" placeholder="Employee ID" required>
+          <div class="input-group-append">
+            <div class="input-group-text">
+              <span class="fas fa-id-badge"></span>
+            </div>
+          </div>
+        </div>
+            <div class="form-group col-6">
+              <select name="type" id="type" class="form-control">
+                <option value="Registrar">Registrar</option>
+                <option value="Accounting">Accounting</option>
+              </select>
+            </div>
+        <div class="input-group mb-3">
+          <input type= "text" name ="fname" class="form-control" placeholder="First Name" required>
+          <div class="input-group-append">
+            <div class="input-group-text">
+              <span class="fas fa-user-circle"></span>
+            </div>
+          </div>
+        </div>
+        <div class="input-group mb-3">
+          <input type= "text" name ="lname" class="form-control" placeholder="Last Name" required>
+          <div class="input-group-append">
+            <div class="input-group-text">
+              <span class="fas fa-user-circle"></span>
+            </div>
+          </div>
+        </div>
+        <div class="input-group mb-3">
+          <input type= "text" name ="username" class="form-control" placeholder="Username" required>
+          <div class="input-group-append">
+            <div class="input-group-text">
+              <span class="fas fa-user"></span>
+            </div>
+          </div>
+        </div>
+        <div class="input-group mb-3">
+          <input type= "password" name ="password" id="password" class="form-control" placeholder="Password" required>
+          <div class="input-group-append">
+            <div class="input-group-text">
+              <span class="fas fa-lock"></span>
+            </div>
+          </div>
+        </div>
+        <div class="input-group mb-3">
+          <input type="password" class="form-control" id="confirmPassword" placeholder="Confirm password" required>
+          <div class="input-group-append">
+            <div class="input-group-text">
+              <span class="fas fa-lock"></span>
+            </div>
+          </div>
+        </div>
+        <div class="row">
+          <!-- /.col -->
+          <div class="col-4">
+            <input input type = "submit" name = "submit" value = "Create" min = "0" class="btn btn-success"/>
+          </div>
+          <!-- /.col -->
+        </div>
+      </form>
+    </div>
+    <!-- /.form-box -->
+  </div><!-- /.card -->
+</div>
+<!-- /.register-box -->
 <!--
 					<div class="wrap-input100 validate-input" data-validate="Employee ID is required">
 						<input class="input100" type="text" name="username" placeholder="Employee ID">
@@ -106,6 +160,20 @@
 		</div>
 	</div>
 
+<script type="text/javascript">
+var password = document.getElementById("password")
+  , confirm_password = document.getElementById("confirmPassword");
+
+function validatePassword(){
+  if(password.value != confirm_password.value) {
+    confirm_password.setCustomValidity("Passwords Don't Match");
+  } else {
+    confirm_password.setCustomValidity('');
+  }
+}
+password.onchange = validatePassword;
+confirm_password.onkeyup = validatePassword;
+</script>
 
 <!--===============================================================================================-->
 	<script src="../Resources/vendor/jquery/jquery-3.2.1.min.js"></script>
