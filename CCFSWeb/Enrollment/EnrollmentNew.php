0364761d7ffae21f0239d61b37300fe436e40943
Paul Perez <45648270+Archaician@users.noreply.github.com>
2020-01-19 19:28:01 +0800

    Search/Autofill working

--- CCFSWeb/Enrollment/EnrollmentNew.php
+++ CCFSWeb/Enrollment/EnrollmentNew.php
@@ -1,22 +1,11 @@
 <?php
-  include("Connection.php");
-  include 'database.php';
+  session_start();
 
-  if(isset($_POST['searcher'])){
-    $f = $_POST['search'];
-    if(is_numeric($f)) {
-      $e = "SELECT SurName,GivenName,MiddleName,gender,birthdate,gradelvl,birthplace,studaddress,homeTelnum,mobilenum,prevschoolattended,faFname,falname,faAdd,faMobilenum,faEmail,faoccupation,moFname,moLname,momobilenum,moAdd,moEmail,mooccupation,sibFname,sibLname,sibBirthdate,sibschoolname,guardianName,guardianAddress,guardianContact From enstudent Where IDno='$f'";
-      $result2 = $conn->query($e);
-      $message = 'E';
-    }else {
-      $e = "SELECT SurName,GivenName,MiddleName,gender,birthdate,gradelvl,birthplace,studaddress,homeTelnum,mobilenum,prevschoolattended,faFname,falname,faAdd,faMobilenum,faEmail,faoccupation,moFname,moLname,momobilenum,moAdd,moEmail,mooccupation,sibFname,sibLname,sibBirthdate,sibschoolname,guardianName,guardianAddress,guardianContact From enstudent Where SurName='$f'";
-      $message = 'Em';
-    }
-  }
+  include('Connection.php');
+  include('database.php');
+
   $query = "SELECT * FROM `section`";
   $result = mysqli_query($mysqli, $query);
-
-  
 ?>
 <html>
 <head>
@@ -59,14 +48,15 @@
       <div class="container-fluid">
         <div class="row mb-2">
           <div class="col-sm-6">
-            <h1 class="m-0 text-dark">Fill up the forms below</h1>
+            <h1 class="text-dark">Fill up the forms below</h1>
+            <br>
               <!-- SEARCH FORM -->
-          <form class="form-inline md-form form-sm mt-0" action="../Enrollment/EnrollmentNew.php" method="post">
-            <i class="fas fa-search" aria-hidden="true"></i>
-            <input class="form-control form-control-sm ml-3 w-75" type="text" placeholder="Search ID or Surname"
-              aria-label="Search" name="search">
-            <input type="submit" name="searcher" value="search">
-          </form>
+            <div class="form-inline">
+              <i class="fas fa-search" aria-hidden="true"></i>
+              <input class="form-control form-control-sm ml-3 w-75" type="text" placeholder="Search ID or Surname"
+                aria-label="Search" name="search">
+              <button type="submit" name="searcher" value="search" class="btn btn-info btn-sm" style="margin-left: 10px;">Search</button>
+            </div>
           </div><!-- /.col -->
           <div class="col-sm-6">
             <ol class="breadcrumb float-sm-right">
@@ -85,12 +75,10 @@
               </div>
               <!-- /.card-header -->
               <!-- form start -->
-              <?php if($_POST['varname'] === 'R') {
-                echo "<form action='../Registrar/RegistrarHome.php' method='post'>";
-              } else {
-                echo "<form action='../Admin/AdminHome.php' method='post'>";
-              }
-              ?>
+              <form action="EnrollmentSave.php" method="post">
+                <input type="hidden" name="studentIDno" value="">
+                <input type="hidden" name="redirect_to" value="<?php echo $_SESSION['TYPE'] == 'ADMIN' ? '../Admin/AdminHome.php' : '../Registrar/RegistrarHome.php'; ?>">
+
                 <div class="card-body">
                   <div class="row">
                   <div class="form-group col-4">
@@ -298,55 +286,6 @@
             <!-- /.card -->
       </div><!-- /.container-fluid -->
     </div>
-
-        <div class="row">
-          <!-- left column -->
-          <div class="col-md-12">
-            <!-- general form elements -->
-            <div class="card card-primary">
-              <div class="card-header">
-                <h3 class="card-title">Sibling Information</h3>
-              </div>
-              <!-- /.card-header -->
-              <!-- form start -->
-              
-                <div class="card-body">
-                  <div class="row">
-                  <div class="form-group col-3">
-                    <label for="siblingSurname">Sibling Surname</label>
-                    <input class="form-control" id="siblingSurname" placeholder="Enter Sibling Surname" name="siblinglast">
-                  </div>
-                  <div class="form-group col-3">
-                    <label for="siblingGivenName">Siblings Given name</label>
-                    <input class="form-control" id="siblingGivenName" placeholder="Enter Sibling Given Name" name="siblingFirst">
-                  </div>
-                  <div class="form-group col-md-3">
-                   <label for="birthDate">Birthdate</label>
-                  <div class="input-group">
-                    <div class="input-group-prepend">
-                      <span class="input-group-text"><i class="far fa-calendar-alt"></i></span>
-                    </div>
-                    <input type="text" class="form-control" data-inputmask-alias="datetime" data-inputmask-inputformat="mm/dd/yyyy" data-mask name="siblingBirth">
-                  </div>
-                </div>
-                  <div class="form-group col-3">
-                    <label for="siblingSchool"> Siblings School</label>
-                    <input class="form-control" id="siblingSchool" placeholder="Enter Address" name="siblingSchoolLast">
-                  </div>
-                  <div class="row">
-                    <button  class="btn btn-primary">Add More</button> 
-                  </div>
-                </div>
-
-           <!-- /.card-body -->
-              </div>
-
-
-
-            </div>
-            <!-- /.card -->
-      </div><!-- /.container-fluid -->
-    </div>
       <div class="card-footer">
           <button type="submit" class="btn btn-success" data-toggle="modal" name="enroll" value="enroll" data-target="#myModal1" style="float: right;">Submit</button>
       </div>
@@ -408,37 +347,6 @@
 <!-- ./wrapper -->
 
 <!-- jQuery -->
-<script>
-  $(function () {
-    //Initialize Select2 Elements
-    $('.select2').select2()
-
-    //Initialize Select2 Elements
-    $('.select2bs4').select2({
-      theme: 'bootstrap4'
-    })
-
-    //Datemask dd/mm/yyyy
-    $('#datemask').inputmask('dd/mm/yyyy', { 'placeholder': 'dd/mm/yyyy' })
-    //Datemask2 mm/dd/yyyy
-    $('#datemask2').inputmask('mm/dd/yyyy', { 'placeholder': 'mm/dd/yyyy' })
-    //Money Euro
-    $('[data-mask]').inputmask()
-
-    //Date range picker
-    $('#reservation').daterangepicker()
-    //Date range picker with time picker
-    $('#reservationtime').daterangepicker({
-      timePicker: true,
-      timePickerIncrement: 30,
-      locale: {
-        format: 'MM/DD/YYYY hh:mm A'
-      }
-    })
-
-  })
-
-</script>
     <script src="../Resources/plugins/jquery/jquery.min.js"></script>
     <!-- jQuery UI 1.11.4 -->
     <script src="../Resources/plugins/jquery-ui/jquery-ui.min.js"></script>
@@ -472,6 +380,89 @@
     <script src="../Resources/dist/js/pages/dashboard.js"></script>
     <!-- AdminLTE for demo purposes -->
     <script src="../Resources/dist/js/demo.js"></script>
+
+    <script type="text/javascript">
+      $('document').ready(function () {
+        $('[name=searcher]').on('click', function () {
+          var search = $('[name=search]').val();
+
+          $.ajax({
+            url: 'searchStudent.php',
+            data: { s: search },
+            dataType: 'json',
+          }).done(function (data) {
+              if ( data.data ) {
+                var student = data.data;
+
+                // name = field name
+                // student. = database column name
+                $('[name=studentIDno]').val(student.IDno);
+                $('[name=studentSurname]').val(student.SurName);
+                $('[name=studentGivenName]').val(student.GivenName);
+                $('[name=studentMiddleName]').val(student.MiddleName);
+                $('[name=gender]').val(student.gender);
+                $('[name=studentBirthDate]').val(student.birthdate);
+                $('[name=gradeLevel]').val(student.gradelvl);
+                $('[name=sename]').val(student.sename);
+                $('[name=studentBirthPlace]').val(student.birthplace);
+                $('[name=studentAddress]').val(student.studaddress);
+                $('[name=studentTelNum]').val(student.homeTelnum);
+                $('[name=studentMobNum]').val(student.mobilenum);
+                $('[name=studentLastSchool]').val(student.prevschoolattended);
+                $('[name=fatherFirst]').val(student.faFname);
+                $('[name=fatherLast]').val(student.falname);
+                $('[name=fatherAdd]').val(student.faAdd);
+                $('[name=fatherOcc]').val(student.faoccupation);
+                $('[name=fatherMobileNum]').val(student.faMobilenum);
+                $('[name=fatherEmailAdd]').val(student.faEmail);
+                $('[name=motherFirst]').val(student.moFname);
+                $('[name=motherLast]').val(student.moLname);
+                $('[name=motherAdd]').val(student.moAdd);
+                $('[name=motherOcc]').val(student.mooccupation);
+                $('[name=motherMobNum]').val(student.momobilenum);
+                $('[name=motherEmAdd]').val(student.moEmail);
+                $('[name=guardianName]').val(student.guardianName);
+                $('[name=guardianAddress]').val(student.guardianAddress);
+                $('[name=guardianContact]').val(student.guardianContact);
+              } else {
+                alert( data.message );
+              }
+          });
+
+          return false;
+        });
+      });
+    </script>
+
+    <script>
+      $(function () {
+        //Initialize Select2 Elements
+        $('.select2').select2()
+
+        //Initialize Select2 Elements
+        $('.select2bs4').select2({
+          theme: 'bootstrap4'
+        })
+
+        //Datemask dd/mm/yyyy
+        $('#datemask').inputmask('dd/mm/yyyy', { 'placeholder': 'dd/mm/yyyy' })
+        //Datemask2 mm/dd/yyyy
+        $('#datemask2').inputmask('mm/dd/yyyy', { 'placeholder': 'mm/dd/yyyy' })
+        //Money Euro
+        $('[data-mask]').inputmask()
+
+        //Date range picker
+        $('#reservation').daterangepicker()
+        //Date range picker with time picker
+        $('#reservationtime').daterangepicker({
+          timePicker: true,
+          timePickerIncrement: 30,
+          locale: {
+            format: 'MM/DD/YYYY hh:mm A'
+          }
+        });
+      });
+    </script>
 </body>
 
 </html>
