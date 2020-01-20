0c21869bd556cef2ce823c099de8848b1d5f8aeb
Imran-D-Mahmood <48464030+Imran-D-Mahmood@users.noreply.github.com>
2020-01-21 02:10:09 +0800

    Delete SectionFetch.php

--- CCFSWeb/Admin/SectionFetch.php
+++
@@ -1,9 +0,0 @@
-<?php
- $connect = mysqli_connect("localhost", "root", "", "ccfs");
- if(isset($_POST["sec_ID"])) {
-   $query = "SELECT * FROM section WHERE secID = '".$_POST["sec_ID"]."'";
-   $result = mysqli_query($connect, $query);
-   $row = mysqli_fetch_array($result);
-   echo json_encode($row);
- }
- ?>
