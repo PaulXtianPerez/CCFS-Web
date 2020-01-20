dc19d702c9d27569876eab6aacb6e9ecdd071bc8
nyaa <37688995+nekoshka@users.noreply.github.com>
2020-01-20 22:37:21 +0800

    remove unused data

--- CCFSWeb/Enrollment/EnrollFees.php
+++ CCFSWeb/Enrollment/EnrollFees.php
@@ -2,9 +2,6 @@
 include('Connection.php');
 $input = filter_input_array(INPUT_POST);
 if(isset($input['idno'])) {
-    if($input['s'] == '') {
-        $input['s'] = 0;
-    }
     $query = "INSERT INTO `feestudent`(tuition, books, misc, service, balance, IDno, yearid) VALUES
     ('".$input['t']."', '".$input['b']."', 
     '".$input['m']."', '".$input['s']."', 
