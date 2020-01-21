ff9d1bfc52312c63524aa2c1b09555a6a78c7d78
Paul Perez <45648270+Archaician@users.noreply.github.com>
2020-01-21 10:39:35 +0800

    Updated

---
+++ CCFSWeb/Admin/StudentFees.php
@@ -0,0 +1,115 @@
+<?php
+// connect to database
+include 'database.php';
+// mysql select query
+$query = "SELECT * FROM `feestudent`";
+// result for method
+$result = mysqli_query($mysqli, $query);
+?>
+<html>
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <title>CCFS Student Information System</title>
+  <link rel="icon" href="../Resources/dist/img/CCFS_logo.png" type="image/icon type">
+  <!-- Tell the browser to be responsive to screen width -->
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <!-- Font Awesome -->
+  <link rel="stylesheet" href="../Resources/plugins/fontawesome-free/css/all.min.css">
+  <!-- Ionicons -->
+  <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
+  <!-- Tempusdominus Bbootstrap 4 -->
+  <link rel="stylesheet" href="../Resources/plugins/tempusdominus-bootstrap-4/css/tempusdominus-bootstrap-4.min.css">
+  <!-- iCheck -->
+  <link rel="stylesheet" href="../Resources/plugins/icheck-bootstrap/icheck-bootstrap.min.css">
+  <!-- JQVMap -->
+  <link rel="stylesheet" href="../Resources/plugins/jqvmap/jqvmap.min.css">
+  <!-- Theme style -->
+  <link rel="stylesheet" href="../Resources/dist/css/adminlte.min.css">
+  <!-- overlayScrollbars -->
+  <link rel="stylesheet" href="../Resources/plugins/overlayScrollbars/css/OverlayScrollbars.min.css">
+  <!-- Daterange picker -->
+  <link rel="stylesheet" href="../Resources/plugins/daterangepicker/daterangepicker.css">
+  <!-- summernote -->
+  <link rel="stylesheet" href="../Resources/plugins/summernote/summernote-bs4.css">
+  <!-- Google Font: Source Sans Pro -->
+  <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700" rel="stylesheet">
+  <link rel="stylesheet" type="text/css" href="../Resources/dist/css/main.css">
+  <!-- CSS for DataTables plugin -->
+  <link rel="stylesheet" type="text/css" href="../Resources/plugins/bootstrap/js/DataTables/datatables.css">
+</head>  
+<body class="hold-transition sidebar-mini layout-fixed">
+<div id="contents" class="wrapper">
+
+  <!-- Content Wrapper. Contains page content -->
+  <div>
+    <!-- Content Header (Page header) -->
+    <div class="content-header">
+      <div class="container-fluid">
+        <div class="row mb-2">
+          <div class="col-sm-6">
+            <h1 class="m-0 text-dark">Student Fees</h1>
+                             
+                    <span id="successmsg"></span>
+                  </div>
+          </div><!-- /.col -->
+        </div><!-- /.row -->
+		</div>
+		</div>
+        <!-- Main content -->
+       <section class="content">
+          
+            <div class="col-12">
+              <div class="card card-primary">
+                <div class="card-header">
+				<div class="input-group input-group-sm">
+				<form class="form-inline ml-1 form-group">
+				  </div>
+				  <div class="input-group input-group-sm">
+						<div class="radio">
+						<label><h4><input type="text" name="id" class="id" placeholder="Enter Id Number"/></h4></label>
+						</div>
+						</div>
+					</form>
+                </div>
+                  <!-- SEARCH FORM -->
+			<!-- Balance -->
+                <!-- /.card-header -->
+                <div class="card-body">
+                  <table id="schyrTable" class="table table-bordered table-hover">
+                    <thead>
+                    <tr>
+					<th>ID Number </th>
+					<th>Books Fee </th>
+					<th>Misc Fee </th>
+					<th>Tuition Fee </th>
+					<th>Service Fee </th>
+					<th>Remaining Balance </th>
+                    </tr>
+                    </thead>
+				<tbody> <!-- Populate from database. -->
+                  <?php while($row = mysqli_fetch_array($result)):;?>
+                    <tr>
+                      <td><?php echo $row["IDno"];?></td>
+                      <td><?php echo $row["books"];?></td>
+                      <td><?php echo $row["misc"];?></td>
+                      <td><?php echo $row["tuition"];?></td>
+                     <td><?php echo $row["balance"];?></td>
+                      <td><input type="button" name="edit" value="Edit" id="<?php echo $row["feestID"]; ?>" class="btn btn-info btn-xs edit_data" /></td>
+                    </tr>
+                  <?php endwhile;?>
+                    </tbody>
+                <!-- /.card-body -->
+              </div>
+              <!-- /.card -->
+            </div>
+          </div>
+        </section>
+      </div><!-- /.container-fluid -->
+    </div>
+  </div>
+</div> <!-- ./wrapper -->
+    </body>  
+</html>  
+
+        
