755e34f0bf42fc5222993925e507693612cdf647
Imran-D-Mahmood <48464030+Imran-D-Mahmood@users.noreply.github.com>
2020-02-11 13:16:30 +0800

    Update session handling

--- CCFSWeb/Accounting/AccountingHome.php
+++ CCFSWeb/Accounting/AccountingHome.php
@@ -7,6 +7,8 @@
      header('location: ../Admin/AdminHome.php');
    }elseif($_SESSION['TYPE']=='REGISTRAR'){
      header('location: ../Registrar/RegistrarHome.php');
+   }elseif($_SESSION['TYPE']=='TEACHER'){
+     header('location: ../Teacher/TeacherHome.php');
    }
  } else {
    header('location: ../index.php');
