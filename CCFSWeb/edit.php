ec251fc7f66422f4efb45e58d47c64717e9efa44
Imran-D-Mahmood <2161367@slu.edu.ph>
2020-02-19 22:32:08 +0800

    Remove unneeded codes

--- CCFSWeb/edit.php
+++ CCFSWeb/edit.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 
 $host = "localhost";
 $user = "root";
@@ -14,42 +14,4 @@ try{
     echo 'Error';
 }
 
-
-
-	if( isset($_GET['edit']) )
-	{
-		$id2 = $_GET['edit'];
-		
-		$res2= "SELECT * FROM reservation WHERE reservation_id='$id2'";
-		$resu2 = mysqli_query($connect, $res2);
-		
-	}
-
-if(isset($_POST['update']))
-{
-    $id2 = $_POST['id2'];
-	$status2 = $_POST['status'];
-    $update_Query2 = "UPDATE `reservation` SET `status`='$status2' WHERE `reservation_id` = '$id2'";
-    try{
-        $update_Result = mysqli_query($connect, $update_Query2);
-        
-        if($update_Result)
-        {
-            if(mysqli_affected_rows($connect) > 0)
-            {
-                echo 'Data Updated';
-            }else{
-                echo 'Data Not Updated';
-            }
-        }
-    } catch (Exception $ex) {
-        echo 'Error Update '.$ex->getMessage();
-    }
-}
-
-
- 
-	
-
 ?>
-
